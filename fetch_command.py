3fea4f2d6a6bf86d471e1bfff39e3711f92dda56
Guruprasad Kamath <guru241987@gmail.com>
2023-03-14 11:43:09 +0100

    update tests generator

---
+++ fetch_command.py
@@ -0,0 +1,33 @@
+import os
+import json
+import argparse
+
+parser = argparse.ArgumentParser(
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+)
+
+parser.add_argument("--testdata", dest="testdata", type=int, required=True)
+
+if __name__ == "__main__":
+    options = parser.parse_args()
+
+    with open("commands.json", "r") as f:
+        cmds = json.load(f)
+
+    key = f"fixtures/expected/{options.testdata}/London.json"
+
+    if key in cmds:
+        value_list = cmds[key]
+        cmd = "evm"
+        for value in value_list["args"]:
+            cmd += " "
+            if "__BASEDIR__" in value:
+                cmd += value.replace("__BASEDIR__", "/Users/guruprasad/Documents/Programming/Ethereum/t8n_testdata")
+            else:
+                cmd += value
+    else:
+        sys.exit(f"Command for {key} not found.")
+
+    print(cmd)
+
+    
